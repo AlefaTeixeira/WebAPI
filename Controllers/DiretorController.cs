d03991e87d197b96a95f744331e5cb5cfcfd51f0
AlefaTeixeira <alefteixeira95@gmail.com>
2021-06-14 21:29:08 -0300

    Novos endpoints

--- Controllers/DiretorController.cs
+++ Controllers/DiretorController.cs
@@ -7,22 +7,25 @@ using Microsoft.EntityFrameworkCore;
 [Route("[controller]")]
 public class DiretorController : ControllerBase {
     private readonly ApplicationDbContext _context;
+
     public DiretorController(ApplicationDbContext context) {
         _context = context;
     }
 
+    // GET api/diretores
     [HttpGet]
     public async Task<List<Diretor>> Get() {
         return await _context.Diretores.ToListAsync();
     }
-     
+
+    // GET api/diretores/1
     [HttpGet("{id}")]
-    public async Task<ActionResult<Diretor>> Get(long id){
-       var diretor = await _context.Diretores.FirstOrDefaultAsync(Diretor => diretor.Id == id);
-       
-       return Ok(diretor);
+    public async Task<ActionResult<Diretor>> Get(long id) {
+        var diretor = await _context.Diretores.FirstOrDefaultAsync(diretor => diretor.Id == id);
+        return Ok(diretor);
     }
 
+    // POST api/diretores
     [HttpPost]
     public async Task<ActionResult<Diretor>> Post([FromBody] Diretor diretor) {
         _context.Diretores.Add(diretor);
@@ -30,20 +33,23 @@ public class DiretorController : ControllerBase {
 
         return Ok(diretor);
     }
-     [HttpPut("{id}")]
-      public async Task<ActionResult<Diretor>> Put(long id, [FromBody] Diretor diretor){
-          diretor.Id = id;
-          _context.Diretores.Update(diretor);
-          await _context.SaveChangesAsync();
 
-          return Ok(diretor);
+    // PUT api/diretores/{id}
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Diretor>> Put(long id, [FromBody] Diretor diretor) {
+        diretor.Id = id;
+        _context.Diretores.Update(diretor);
+        await _context.SaveChangesAsync();
+
+        return Ok(diretor);
     }
-        [HttpDelete("{id}")]
-         public async Task<ActionResult> Delete(long id){
-            var diretor = await _context.Diretores.FirstOrDefaultAsync(Diretor => diretor.Id == id);
-            _context.Remove(diretor);
-            _context.SaveChangesAsync();
 
-            return Ok(diretor);
+    // DELETE api/diretores/{id}
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(long id) {
+        var diretor = await _context.Diretores.FirstOrDefaultAsync(diretor => diretor.Id == id);
+        _context.Remove(diretor);
+        await _context.SaveChangesAsync();
+        return Ok(diretor);
     }
 }
