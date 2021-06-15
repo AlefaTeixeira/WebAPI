8fabbb7522b4c298babf755d49c292fe106a146a
AlefaTeixeira <alefteixeira95@gmail.com>
2021-06-14 21:20:31 -0300

    Tarefa_3

--- Controllers/DiretorController.cs
+++ Controllers/DiretorController.cs
@@ -15,6 +15,13 @@ public class DiretorController : ControllerBase {
     public async Task<List<Diretor>> Get() {
         return await _context.Diretores.ToListAsync();
     }
+     
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Diretor>> Get(long id){
+       var diretor = await _context.Diretores.FirstOrDefaultAsync(Diretor => diretor.Id == id);
+       
+       return Ok(diretor);
+    }
 
     [HttpPost]
     public async Task<ActionResult<Diretor>> Post([FromBody] Diretor diretor) {
@@ -22,5 +29,21 @@ public class DiretorController : ControllerBase {
         await _context.SaveChangesAsync();
 
         return Ok(diretor);
+    }
+     [HttpPut("{id}")]
+      public async Task<ActionResult<Diretor>> Put(long id, [FromBody] Diretor diretor){
+          diretor.Id = id;
+          _context.Diretores.Update(diretor);
+          await _context.SaveChangesAsync();
+
+          return Ok(diretor);
+    }
+        [HttpDelete("{id}")]
+         public async Task<ActionResult> Delete(long id){
+            var diretor = await _context.Diretores.FirstOrDefaultAsync(Diretor => diretor.Id == id);
+            _context.Remove(diretor);
+            _context.SaveChangesAsync();
+
+            return Ok(diretor);
     }
 }
