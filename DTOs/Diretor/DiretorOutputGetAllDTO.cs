ccbb0035487f33b5325dbc72d2734da564bb800c
AlefaTeixeira <alefteixeira95@gmail.com>
2021-06-28 20:32:52 -0300

    Dtos para GetById e diretor

---
+++ DTOs/Diretor/DiretorOutputGetAllDTO.cs
@@ -0,0 +1,11 @@
+public class DiretorOutputGetAllDTO {
+
+    public long Id { get; private set; }
+    public string Nome { get; private set; }
+
+    public DiretorOutputGetAllDTO(long id, string nome) {
+
+        Id = id;
+        Nome = nome;
+    }
+}
